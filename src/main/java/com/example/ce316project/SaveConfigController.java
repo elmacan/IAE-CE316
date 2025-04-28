52bda552f52b36001e926e5225a1ce5db935018b
Elif Sude Ozmen <elifsude035@gmail.com>
2025-04-29 01:01:54 +0300

    save config button actions

--- src/main/java/com/example/ce316project/SaveConfigController.java
+++ src/main/java/com/example/ce316project/SaveConfigController.java
@@ -1,4 +1,26 @@
 package com.example.ce316project;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class SaveConfigController {
+    @FXML private Button cancelButton;
+    @FXML
+    private void handleCancel(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/ce316project/ListConfig.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) cancelButton.getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
