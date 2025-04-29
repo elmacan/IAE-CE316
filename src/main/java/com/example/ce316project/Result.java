954126317e70a98e768cc81ac9bb253b478b2156
aleyna <aleynakok8c@gmail.com>
2025-04-29 22:20:02 +0300

    getter and setters of Result

--- src/main/java/com/example/ce316project/Result.java
+++ src/main/java/com/example/ce316project/Result.java
@@ -6,4 +6,49 @@ public class Result {
    private boolean outputMatches;
    private String errorLog;
 
+
+
+   public boolean isCompiledSuccessfully() {
+      return compiledSuccessfully;
+   }
+
+   public void setCompiledSuccessfully(boolean compiledSuccessfully) {
+      this.compiledSuccessfully = compiledSuccessfully;
+   }
+
+   public boolean isRunSuccessfully() {
+      return runSuccessfully;
+   }
+
+   public void setRunSuccessfully(boolean runSuccessfully) {
+      this.runSuccessfully = runSuccessfully;
+   }
+
+   public boolean isOutputMatches() {
+      return outputMatches;
+   }
+
+   public void setOutputMatches(boolean outputMatches) {
+      this.outputMatches = outputMatches;
+   }
+
+   public String getErrorLog() {
+      return errorLog;
+   }
+
+   public void setErrorLog(String errorLog) {
+      this.errorLog = errorLog;
+   }
+
+   @Override
+   public String toString() {
+      return "Result{" +
+              "compiledSuccessfully=" + compiledSuccessfully +
+              ", runSuccessfully=" + runSuccessfully +
+              ", outputMatches=" + outputMatches +
+              ", errorLog='" + errorLog + '\'' +
+              '}';
+   }
 }
+
+
