820984d009e726ae06710a7326a271950aebd1df
elmacan <betozsan@hotmail.com>
2025-05-02 17:15:30 +0300

    ButtonAction for select StudentSubmissions Directory

--- src/main/java/com/example/ce316project/CreateProjectController.java
+++ src/main/java/com/example/ce316project/CreateProjectController.java
@@ -8,10 +8,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.File;
 import java.net.URL;
@@ -22,6 +26,9 @@ public class CreateProjectController implements Initializable {
 
     @FXML
     private TextField projectNameField, sourceFileField, expectedOutputFileField;
+
+    @FXML
+    private Button browseSourceButton;
 
     @FXML
     private TextArea argumentsArea;
@@ -35,6 +42,7 @@ public class CreateProjectController implements Initializable {
         if (IAEController.configurationList != null) {
             configurationComboBox.setItems(FXCollections.observableArrayList(IAEController.configurationList));
         }
+        browseSourceButton.setOnAction(this::chooseZipFileDirectory);
     }
     @FXML
     private void onCancelButton(ActionEvent event) {
@@ -79,5 +87,24 @@ public class CreateProjectController implements Initializable {
         }
     }
 
+    @FXML
+    private void chooseZipFileDirectory(ActionEvent event) {
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select Submission Directory");
+
+        Window window = ((Node) event.getSource()).getScene().getWindow();
+        File selectedDirectory = directoryChooser.showDialog(window);
+
+        if (selectedDirectory != null) {
+            sourceFileField.setText(selectedDirectory.getAbsolutePath());
+        }else{
+            javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText(null);
+            alert.setContentText("Please select a directory!");
+            alert.showAndWait();
+        }
+    }
 
 }
+
